5d7b885bc2281779bf8a8fe00f86e41abf6ca3a4
truong21 <543@holbertonschool.com>
2020-06-25 02:19:07 +0000

    utf-8 validation

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def validUTF8(data):
+    count = 0
+    for num in data:
+        mask = 0b10000000
+        if not count:
+            while (mask & num):
+                count += 1
+                mask >>= 1
+            if count > 5:
+                return False
+            if count:
+                count -= 1
+                if count == 0:
+                    return False
+        elif count > 0:
+            if num >> 6 != 2:
+                return False
+            count -= 1
+    return not count
